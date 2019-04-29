11af0bd9894bd586bf6ecb71654d14b21a120d7a
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-04-29 17:00:33 +0200

    going forward works, going back does not

--- ioctl.c
+++ ioctl.c
@@ -17,14 +17,21 @@ int main(int argc, char const *argv[]) {
   if(fd != 0)
   {
     bool end_of_file = false;
+    long last_page_position;
     do
     {
+      char input = getchar();
+      if(input == 'b')
+      {
+        fseek(fd, last_page_position, SEEK_SET);
+      }
       for(int o = 0; o < ws.ws_row - 2; o++)
       {
         for(int i = 0; i < ws.ws_col; i++)
         {
+          last_page_position = ftell(fd);
           int output = fgetc(fd);
-          if(output != -1)
+          if(output != -1)    // If end of file is reached
           {
             printf("%c", output);
           }
@@ -33,15 +40,13 @@ int main(int argc, char const *argv[]) {
               end_of_file = true;
           }
         }
-        if(!end_of_file)
-        { printf("\n"); }
       }
-      getchar();
+      if(!end_of_file)
+      { printf("To go forward press enter.\n"); }
     } while(!end_of_file);
   }
   else
-  {
-      printf("ERROR\n");
-  }
+  { printf("ERROR, File not found\n"); }
+  fclose(fd);
   return 0;
 }
